3b60e050f147a38fe55f53375d8578c41f4b0cfc
ISHII 2bit <ishii@buffer-renaiss.com>
2017-01-19 21:05:54 +0900

    separate test with describe

--- test/functional/object.spec.js
+++ test/functional/object.spec.js
@@ -12,60 +12,78 @@ describe('bbb.functional.object:', () => {
         c: 400
     };
 
-    it('object.forEach', () => {
-        expect(object.forEach).toBeDefined();
-
-        const result = object.forEach(source, (key, value) => {
-            expect(value).toEqual(source[key]);
+    describe('bbb.functional.object.forEach', () => {
+        it('exist forEach', () => {
+            expect(object.forEach).toBeDefined();
         });
-        expect(result).toEqual(source);
+        it('object.forEach', () => {
+            const result = object.forEach(source, (key, value) => {
+                expect(value).toEqual(source[key]);
+            });
+            expect(result).toEqual(source);
+        });
     });
 
-    it('object.map', () => {
-        expect(object.map).toBeDefined();
-
-        const result = object.map(source, (key, value) => {
-            if(key == 'a') return -100;
-            return value * 2;
+    describe('bbb.functional.object.map', () => {
+        it('exist map', () => {
+            expect(object.map).toBeDefined();
         });
-        expect(result.a).toBeDefined();
-        expect(result.a).toEqual(-100);
+        it('object.map', () => {
+            const result = object.map(source, (key, value) => {
+                if(key == 'a') return -100;
+                return value * 2;
+            });
 
-        expect(result.b).toBeDefined();
-        expect(result.b).toEqual(400);
+            expect(result).toBeDefined();
 
-        expect(result.c).toBeDefined();
-        expect(result.c).toEqual(800);
+            expect(result.a).toBeDefined();
+            expect(result.a).toEqual(-100);
+
+            expect(result.b).toBeDefined();
+            expect(result.b).toEqual(400);
+
+            expect(result.c).toBeDefined();
+            expect(result.c).toEqual(800);
+        });
     });
 
-    it('object.mapKV', () => {
-        expect(object.mapKV).toBeDefined();
-
-        const result = object.mapKV(source, (key, value) => {
-            return {
-                key: "new_" + key,
-                value: value * 2
-            };
+    describe('bbb.functional.object.mapKV', () => {
+        it('exist mapKV', () => {
+            expect(object.mapKV).toBeDefined();
         });
-        
-        expect(result.new_a).toBeDefined();
-        expect(result.new_a).toEqual(200);
+        it('object.mapKV', () => {
+            const result = object.mapKV(source, (key, value) => {
+                return {
+                    key: "new_" + key,
+                    value: value * 2
+                };
+            });
 
-        expect(result.new_b).toBeDefined();
-        expect(result.new_b).toEqual(400);
+            expect(result).toBeDefined();
+            
+            expect(result.new_a).toBeDefined();
+            expect(result.new_a).toEqual(200);
 
-        expect(result.new_c).toBeDefined();
-        expect(result.new_c).toEqual(800);
+            expect(result.new_b).toBeDefined();
+            expect(result.new_b).toEqual(400);
+
+            expect(result.new_c).toBeDefined();
+            expect(result.new_c).toEqual(800);
+        });
     });
 
-    it('object.reduce', () => {
-        expect(object.reduce).toBeDefined();
-
-        const result = object.reduce(source, (key, value, reduced) => {
-            if(key == 'a') return reduced - value;
-            return reduced + value;
-        }, 50);
-        expect(result).toBeDefined();
-        expect(result).toEqual(50 - source.a + source.b + source.c);
+    describe('bbb.functional.object.reduce', () => {
+        it('exist reduce', () => {
+            expect(object.reduce).toBeDefined();
+        });
+        it('object.reduce', () => {
+            const result = object.reduce(source, (key, value, reduced) => {
+                if(key == 'a') return reduced - value;
+                return reduced + value;
+            }, 50);
+            
+            expect(result).toBeDefined();
+            expect(result).toEqual(50 - source.a + source.b + source.c);
+        });
     });
 });
