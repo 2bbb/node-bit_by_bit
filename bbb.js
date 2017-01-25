38ae3b149e803421c450a86c7d669fe8a03ffd53
ISHII 2bit <ishii@buffer-renaiss.com>
2017-01-25 18:36:03 +0900

    add container and requier in main

--- bbb.js
+++ bbb.js
@@ -2,5 +2,7 @@ const lazier = require('lazier');
 
 module.exports = lazier({
 	functional: './lib/functional.js',
-	osc: './lib/osc.js'
+	osc: './lib/osc.js',
+	math: './lib/math.js',
+	container: './lib/container.js'
 }, __dirname);
