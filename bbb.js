fb6ea7199124d2370c0d822b7c5c5ebe7351ca34
ISHII 2bit <ishii@buffer-renaiss.com>
2017-02-16 18:35:59 +0900

    make clean with lazier

--- bbb.js
+++ bbb.js
@@ -1,9 +1,6 @@
 const lazier = require('lazier');
 
-module.exports = lazier({
-	functional: './lib/functional.js',
-	osc: './lib/osc.js',
-	math: './lib/math.js',
-	container: './lib/container.js',
-	esc: './lib/esc.js'
-}, __dirname);
+const bbb = lazier.from('./lib', __dirname);
+bbb.lazier = lazier;
+
+module.exports = bbb;
